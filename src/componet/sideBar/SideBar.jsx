16ab40faf50fccb0b7c8db1a7a560c986b1136ed
Amir <amirnekoonam85@gmail.com.>
2024-10-24 01:51:45 +0330

    fix2

--- src/componet/sideBar/SideBar.jsx
+++ src/componet/sideBar/SideBar.jsx
@@ -1,31 +1,32 @@
-import { Box, Button, Flex, Image } from '@chakra-ui/react'
-import React from 'react'
-import pix from '../../../public/logo.png'
-import { useNavigate } from 'react-router-dom'
+import { Box, Button, Flex, Image } from '@chakra-ui/react';  
+import React from 'react';  
+import pix from '../../../public/logo.png'; // Ensure the path is correct  
+import { useNavigate } from 'react-router-dom';  
 
-function SideBar() {
-  const useNavigate=useNavigate()
+function SideBar() {  
+  const navigate = useNavigate(); // Fixed variable name  
 
-  function clickHand(){
-    useNavigate('/auth')
-  }
-  return (
-   <Box
-   h={"100vh"}
-   borderRight={"1px solid"}
-   borderColor={"whiteAlpha.300"}
-   py={8}
-   position={"sticky"}
-   top={0}
-   left={0}
-   px={{base:0,md:4}}>
-  
-   <Flex direction={"column"} gap={5} w={'full'} height={'full'}>
-    {/* <Image src={'../../../public/logo.png'}></Image> */}
-   </Flex>
-        <Button onClick={clickHand}>click me</Button>
-   </Box>
-  )
-}
+  function clickHand() {  
+    navigate('/auth'); // Use the correct function here  
+  }  
 
-export default SideBar
+  return (  
+    <Box  
+      h={"100vh"}  
+      borderRight={"1px solid"}  
+      borderColor={"whiteAlpha.300"}  
+      py={8}  
+      position={"sticky"}  
+      top={0}  
+      left={0}  
+      px={{ base: 0, md: 4 }}  
+    >  
+      <Flex direction={"column"} gap={5} w={'full'} height={'full'}>  
+        <Image src={pix} /> {/* Uncommented and fixed to use the imported image */}  
+      </Flex>  
+      <Button  onClick={clickHand }>Log out</Button>  
+    </Box>  
+  );  
+}  
+
+export default SideBar;
